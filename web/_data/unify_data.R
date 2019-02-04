0ca4295739ba5fb1bcdbcb1ba00f9e4c06c6ed95
Nacho <nacho@air>
2019-02-04 12:30:01 +0100

    Unify data

--- web/_data/unify_data.R
+++ web/_data/unify_data.R
@@ -5,6 +5,26 @@
 gh <- read.csv("web/_data/grasshoppers.csv")
 head(gh)
 
+qp <- read.csv("web/_data/data_chiroptera.csv")
+head(qp)
+
+bf <- read.csv("web/_data/lepidoptera.csv", sep = ";")
+head(bf)
+colnames(bf) <- c("species",
+                  "family",
+                  "decimalLatitude",
+                  "decimalLongitude",
+                  "datetime")
+
+to_select <- c("species",
+               "family",
+               "decimalLatitude",
+               "decimalLongitude")
+
+temp <- rbind(gh, qp)
+data <- rbind(temp[,to_select],bf[,to_select])
+head(data)
+dim(data)
 
 #save on data.csv
 
